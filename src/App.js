de06ad7ba15d29d005631a993c19a22712d5df1a
savaleks <savaleks@gmail.com>
2020-11-05 08:35:29 +0200

    updated menu-item to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>KEPURES PUSLAPIS</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/kepures' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,29 +13,34 @@ class Directory extends React.Component {
         {
           title: 'kepures',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1
+          id: 1,
+          linkUrl: 'kepures'
         },
         {
           title: 'striukes',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2
+          id: 2,
+          linkUrl: 'striukes'
         },
         {
           title: 'batai',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3
+          id: 3,
+          linkUrl: 'batai'
         },
         {
           title: 'moteriski',
           imageUrl: 'https://i.ibb.co/0MjBR6k/woman-2593366-1920.jpg',
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl: 'moteriski'
         },
         {
           title: 'vyriski',
           imageUrl: 'https://i.ibb.co/WGKSYtZ/man-1150058-1920.jpg',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: 'vyriski'
         }
       ]
     };
@@ -44,8 +49,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({id, ...otherProps}) => (
+          <MenuItem key={id} {...otherProps} />
         ))}
       </div>
     );
